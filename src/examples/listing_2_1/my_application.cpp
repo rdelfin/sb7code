5642690df98936542a166d2ba999b51ebcbdce6f
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-09-04 21:14:38 +0100

    Added listing 2.2

--- src/examples/listing_2_1/my_application.cpp
+++ src/examples/listing_2_1/my_application.cpp
@@ -3,7 +3,7 @@
 class my_application : public sb7::application {
 public:
     void render(double currentTime) {
-        static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 0.0f };
+        static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
         glClearBufferfv(GL_COLOR, 0, red);
     }
 };
---
+++ src/examples/listing_2_2/my_application.cpp
@@ -0,0 +1,15 @@
+#include "sb7.h"
+
+#include <iostream>
+
+class my_application : public sb7::application {
+public:
+    void render(double currentTime) {
+        const GLfloat color[] = { (float)sin(currentTime) * 0.5f + 0.5f,
+                                  (float)cos(currentTime) * 0.5f + 0.5f,
+                                  0.0f, 1.0f };
+        glClearBufferfv(GL_COLOR, 0, color);
+    }
+};
+
+DECLARE_MAIN(my_application)
